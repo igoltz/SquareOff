6a906c4e6e52a9d64736967ca16822e0bf4e5b6b
Goltz, Immo <immo.goltz@goethe.de>
2023-01-10 13:20:47 +0100

    force push for pebe demo

--- common/config.js
+++ common/config.js
@@ -49,6 +49,7 @@ const config = {
     TICK_FAST_INTERVAL: 50,
     WINNING_SCORE: 7,
     //WINNING_SCORE: process.env.WINNING_SCORE || 5,
+    // set env in deployment!
     WIN_SCREEN_TIMEOUT: 7000,  // time that the player stays on the win/loss scree before starting new game
     MAX_PLACED_BLOCKS: 3,
     MAX_INACTIVE_TIME: 45000,
